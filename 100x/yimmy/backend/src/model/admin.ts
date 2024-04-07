9fb8f381ae46a381475d5274675e460766b8aae7
FossXpert <FossXpert@outlook.com>
2024-04-08 02:04:56 +0530

    jj yes

--- 100x/yimmy/backend/src/model/admin.ts
+++ 100x/yimmy/backend/src/model/admin.ts
@@ -1,1 +1,21 @@
-import mongoose from "mongoose";
+import mongoose,{Document,Schema} from "mongoose";
+
+interface IAdmin extends Document {
+    isAdmin : boolean,
+    firstName?: string,
+    lastName?: string,
+    email?: string,
+    userName?: string,
+    password : string
+}
+
+//Define mongoose Schema
+
+const AdminSchema = new Schema({
+    isAdmin: {type: Boolean, required: true},
+    firstName: String,
+    lastName: String,
+    email:{ type:String , unique:true ,required: [true,"Why no `email`?"] },
+    userName: { type: String ,unique:true  } ,
+    password: { type: String , required: [true,"Why no `password`?"]}
+});
