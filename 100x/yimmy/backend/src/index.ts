6920f4def6d2cf44d38ac6b5af464820ff198b7e
FossXpert <FossXpert@outlook.com>
2024-04-08 00:36:43 +0530

    Add kareem as a dependency, update package-lock.json and package.json, and import kareem in index.ts
    This diff adds kareem as a dependency in package.json with version 2.6.3, updates package-lock.json accordingly, and imports kareem in index.ts. However, there are no further changes to the code in index.ts or any other files.

---
+++ 100x/yimmy/backend/src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const port : number = 3001
+const app: express.Application = express();
+app.use(bodyParser.json());
+// Enable CORS for all domains
+app.use(cors())
+
+const connectionStringLocal: string = "mongodb://localhost:27017/";
+const connectionString: string = "mongodb+srv://rahulray8518:rahulray85188101@cluster0.oveeh21.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(connectionStringLocal);
+mongoose.connection.on('connected', () => {
+    console.log("mongoose Connected");
+});
+mongoose.connection.on('disconnected', () => {
+    console.log("mongoose Disconnected");
+});
+mongoose.connection.on('error', (err: Error) => {
+    console.log(`mongoose Error: ${err}`);
+});
+
+app.use()
+app.listen(port,"100.93.3.137",() => {
+    console.log(`Backend is running on PORT : ${port}`)
+})
