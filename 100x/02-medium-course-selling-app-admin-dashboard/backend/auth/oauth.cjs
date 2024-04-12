89b3639d4ee5cc9b7fa5119285ba7f1128b2b9e4
FossXpert <FossXpert@outlook.com>
2024-04-12 16:04:08 +0530

    c

--- 100x/02-medium-course-selling-app-admin-dashboard/backend/auth/oauth.cjs
+++ 100x/02-medium-course-selling-app-admin-dashboard/backend/auth/oauth.cjs
@@ -38,7 +38,7 @@ passport.deserializeUser(function(obj, done) {
 });
 
 /* Google AUTH */
-c
+
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const GOOGLE_CLIENT_ID = 'our-google-client-id';
 const GOOGLE_CLIENT_SECRET = 'our-google-client-secret';
