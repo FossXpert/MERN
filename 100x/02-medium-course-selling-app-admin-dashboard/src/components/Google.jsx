4920114247313f848720394d4100d0d7bfc36417
FossXpert <FossXpert@outlook.com>
2024-04-14 00:57:45 +0530

    Auto commit: Updated files: 100x/02-medium-course-selling-app-admin-dashboard/src/components/Google.jsx

--- 100x/02-medium-course-selling-app-admin-dashboard/src/components/Google.jsx
+++ 100x/02-medium-course-selling-app-admin-dashboard/src/components/Google.jsx
@@ -5,7 +5,7 @@ export function Google(){
         <h1>Google</h1>
         <p>Welcome to the Googlestack! This is a simple website built using React.js and utilizes the Google Books API for book search functionality.</p>
         <p>Welcome to the Googlestack! This is a website for learning about how to use google products and services.</p>
-        <Button>Sign In With Google</Button>
+        <Button>Signin with Google</Button>
      </div>
 }
 
