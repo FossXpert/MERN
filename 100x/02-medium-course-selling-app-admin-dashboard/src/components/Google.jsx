5ff6fb4366d5f754b11473ba5e93fed5e15017b6
FossXpert <FossXpert@outlook.com>
2024-04-14 09:29:25 +0530

    Auto commit: Updated files: 100x/02-medium-course-selling-app-admin-dashboard/src/components/Google.jsx

--- 100x/02-medium-course-selling-app-admin-dashboard/src/components/Google.jsx
+++ 100x/02-medium-course-selling-app-admin-dashboard/src/components/Google.jsx
@@ -1,12 +1,13 @@
 import { Button, Card, TextField } from "@mui/material";
 
 export function Google(){
+    const handleGoogle = () => {
+        
+    }
     return <div>
         <h1>Google</h1>
         <p>Welcome to the Googlestack! This is a simple website built using React.js and utilizes the Google Books API for book search functionality.</p>
         <p>Welcome to the Googlestack! This is a website for learning about how to use google products and services.</p>
-        <Button>Signin with Google</Button>
+        <Button onClick={handleGoogle}>Signin with Google</Button>
      </div>
 }
-
-6
