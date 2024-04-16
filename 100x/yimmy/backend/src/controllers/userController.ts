7ad6703c9b1af096cc96c49a3cf5b7782e2f1b4b
FossXpert <FossXpert@outlook.com>
2024-04-16 18:07:31 +0530

    Auto commit: Updated files: 100x/yimmy/backend/src/controllers/userController.ts

--- 100x/yimmy/backend/src/controllers/userController.ts
+++ 100x/yimmy/backend/src/controllers/userController.ts
@@ -5,28 +5,32 @@ import { Request, Response } from 'express';
 import { Jwt } from 'jsonwebtoken';
 
 require('dotenv').config()
+
+const validateEmailID = async(email:string) : Promise<boolean> =>{
+    let isEmailExist : User | null = await UserModel.findOne({ email });
+    return !!isEmailExist;
+};
+const validateUserName = async(username:string) : Promise<boolean> =>{
+    let isUsernameExist : User | null = await UserModel.findOne({ username });
+    return !!isUsernameExist; 
+};
+const encryptPassword = async(password: string):Promise<string>=>{
+    return await  bcrypt.hash(password ,12);
+};
+const decryptPassword = async()
 const userSignup = async(req:Request,role:string,res:Response) => {
     try {
         //validate Email ID
-        const validateEmailID = async(email:string) : Promise<boolean> =>{
-            let isEmailExist : User | null = await UserModel.findOne({ email });
-            return !!isEmailExist;
-        };
         if (await validateEmailID(req.body.email)) {
             res.status(409).json('This email id already exist');
             return;
         }
         //validating Username
-        const validateUserName = async(username:string) : Promise<boolean> =>{
-            let isUsernameExist : User | null = await UserModel.findOne({ username });
-            return !!isUsernameExist; 
-        };
         if (await validateUserName(req.body.username)) {
             res.status(422).send("The username you have entered is not valid");
             return;
         }
         //save password using bcrypt
-
         const password = await bcrypt.hash(req.body.password , 12);
         const user = new UserModel({
             ...req.body,
