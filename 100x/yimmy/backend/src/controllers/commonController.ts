4b6d0575bbb6537d22ccca685da5510e641a38a9
FossXpert <FossXpert@outlook.com>
2024-04-16 13:48:45 +0530

    Auto commit: Updated files: 100x/yimmy/backend/src/controllers/commonController.ts

--- 100x/yimmy/backend/src/controllers/commonController.ts
+++ 100x/yimmy/backend/src/controllers/commonController.ts
@@ -8,7 +8,7 @@ const userSignup = async(req:Request,role:string,res:Response) => {
     try {
         //Get user from database of same name if any
         const validateUser = async(email:string) =>{
-            let user = await  User.findOne({ email });
+            let user = await User.findOne({ email });
             
         }
     } catch (error) {
