632a78889ca2807e83bf48fcc73ef3e723eac430
FossXpert <FossXpert@outlook.com>
2024-04-16 13:08:35 +0530

    Auto commit: Updated files: 100x/yimmy/backend/src/controllers/commonController.ts

--- 100x/yimmy/backend/src/controllers/commonController.ts
+++ 100x/yimmy/backend/src/controllers/commonController.ts
@@ -1,4 +1,10 @@
-import bcrypt from 'bcrypt'
-const user = require('../models/User')
-const admin = require('../models/Admin')
+import bcrypt from 'bcrypt';
+import { User } from '../models/User';
+import { Admin } from '../models/Admin';
 
+
+module.exports = {
+    const Signup = async()=>{
+        
+    }
+}
