ec1c36600cc3decc86b92bf9bae1634645359ca2
FossXpert <FossXpert@outlook.com>
2024-04-16 13:18:38 +0530

    Auto commit: Updated files: 100x/yimmy/backend/src/controllers/commonController.ts

--- 100x/yimmy/backend/src/controllers/commonController.ts
+++ 100x/yimmy/backend/src/controllers/commonController.ts
@@ -1,10 +1,16 @@
 import bcrypt from 'bcrypt';
 import { User } from '../models/User';
 import { Admin } from '../models/Admin';
+import { Request, Response } from 'express';
 
 
-module.exports = {
-    const Signup = async()=>{
+const userSignup = async(req:Request,role:string,res:Response) => {
+    try {
+        //Get user from database of same name if any
+        const validateUser = async(email:string) =>{
+            let user = await  User.findOne({ email });
+        }
+    } catch (error) {
         
     }
 }
