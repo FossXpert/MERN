317726746661512be007de9f19bce830f3eacd54
FossXpert <FossXpert@outlook.com>
2024-04-16 15:08:51 +0530

    Auto commit: Updated files: 100x/yimmy/backend/src/controllers/commonController.ts

--- 100x/yimmy/backend/src/controllers/commonController.ts
+++ 100x/yimmy/backend/src/controllers/commonController.ts
@@ -7,9 +7,15 @@ import { Request, Response } from 'express';
 const userSignup = async(req:Request,role:string,res:Response) => {
     try {
         //Get user from database of same name if any
-        const validateUser = async(email:string) =>{
-            let user = await User.findOne({ email });
-            return  user;
+        const validateEmailID = async(email:string) =>{
+            let isEmailExist = await User.findOne({ email });
+            return isEmailExist?true:false;
+        };
+
+        //validating emailID
+        const validateUserName = async(username:string) =>{
+            let isUsernameExist = await User.findOne({ username });
+            return isUsernameExist?true:false;
         };
     } catch (error) {
         
