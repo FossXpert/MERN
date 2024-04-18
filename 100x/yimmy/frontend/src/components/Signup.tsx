af53d7be26171ba6e1010ef5f39c8e741e54a664
FossXpert <FossXpert@outlook.com>
2024-04-18 18:16:57 +0530

    Change Signup.tsx: update Copyright props type to TypographyProps<'span'>
    The diff updates the props type of the Copyright function in Signup.tsx from 'any' to 'TypographyProps<"span">'.

--- 100x/yimmy/frontend/src/components/Signup.tsx
+++ 100x/yimmy/frontend/src/components/Signup.tsx
@@ -10,10 +10,10 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
+import Typography, {TypographyProps } from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Copyright(props: any) {
+function Copyright(props: TypographyProps<'span'>) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
