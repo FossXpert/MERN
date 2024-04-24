43c4bc61303a7b35a60d4376593292348ae5d78d
FossXpert <FossXpert@outlook.com>
2024-04-24 17:57:09 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/App.tsx

--- 100x/yimmy/frontend/src/App.tsx
+++ 100x/yimmy/frontend/src/App.tsx
@@ -2,17 +2,15 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import './App.css';
 import SignIn from './views/SignIn';
 
-
-const domain = "dev-cd616eaxtu7so5dm.us.auth0.com"
-const clientId = "Taa9lZEEXS6JSSGDViJVwwtJunorSJ0D"
-
 function App() {
   return (
     <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    authorizationParams={{ redirect_uri: window.location.origin }}
-    >
+    domain="dev-cd616eaxtu7so5dm.us.auth0.com"
+    clientId="Taa9lZEEXS6JSSGDViJVwwtJunorSJ0D"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
       <div>
         <SignIn />
       </div>
