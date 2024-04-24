84945afeac3a8464b13116b56ccd5582ef6b3787
FossXpert <FossXpert@outlook.com>
2024-04-24 13:36:53 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/App.tsx

--- 100x/yimmy/frontend/src/App.tsx
+++ 100x/yimmy/frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import './App.css'
-import SignIn from './views/SignIn'
+import { Auth0Provider } from '@auth0/auth0-react';
+import './App.css';
+import SignIn from './views/SignIn';
 
 function App() {
-  return <div>
-    <SignIn/>
-  </div>
+  return (
+    <Auth0Provider
+      domain="dev-cd616eaxtu7so5dm.us.auth0.com"
+      clientId="Taa9lZEEXS6JSSGDViJVwwtJunorSJ0D"
+      authorizationParams= {{redirect_uri: window.location.origin}}
+    >
+      <div>
+        <SignIn />
+      </div>
+    </Auth0Provider>
+  );
 }
 
-export default App
+export default App;
