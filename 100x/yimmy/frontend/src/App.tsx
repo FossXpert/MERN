e37e266a322ed486c83712809848933c0731cd69
FossXpert <FossXpert@outlook.com>
2024-04-25 09:57:26 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/App.tsx

--- 100x/yimmy/frontend/src/App.tsx
+++ 100x/yimmy/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SignIn from './views/SignIn';
+import Home from './views/Home';
 
 function App() {
   return (
