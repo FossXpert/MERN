5cc3cefabe98d99a8a1571645fa4df06963255f6
FossXpert <FossXpert@outlook.com>
2024-04-25 16:36:27 +0530

    This the wroking code for backend with customSigIn, after this I an gonna change it to use Auth0 as login service provider. Please reset from here if u wanna restore this version

--- 100x/yimmy/frontend/src/views/Home.tsx
+++ 100x/yimmy/frontend/src/views/Home.tsx
@@ -2,15 +2,31 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LogIn from "../components/Login";
 import Logout from "../components/Logout";
 import Profile from "../components/Profile";
+import { Button } from "@mui/material";
+import axios from "axios";
 
 
 export default function Home(){
     const {isAuthenticated,user,error,isLoading} = useAuth0();
     console.log("isAuthenticated",isAuthenticated)
     console.log(user)
+
+    const callApi = async() =>{
+        try {
+            const url = 
+            const response =  await axios.get("")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <main className="column">
             <h1>Auth0 Login</h1>
+            <ul>
+                <li><Button>Protected Route</Button></li>
+                <li><Button>Unprotected Route</Button></li>
+            </ul>
             {error && <p>Authentication Error Mate</p>}
             {!error && isLoading && <p> Loading...</p>}
             {!error && !isLoading && (
