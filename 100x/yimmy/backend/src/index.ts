c1ded88b69595781b606d85cbe05c3f71a6b7749
FossXpert <FossXpert@outlook.com>
2024-04-25 19:53:19 +0530

    Remove Auth0 and add express-oauth2-jwt-bearer for user route protection
    The `useAuth0` and related imports have been removed from the `oauthJwtHandler.ts` file. Instead, the

--- 100x/yimmy/backend/src/index.ts
+++ 100x/yimmy/backend/src/index.ts
@@ -2,14 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-import userRouter from "./routes/userRoutes";
+import userRoutes from "./routes/userRoutes";
 import dotenv from 'dotenv';
+import { useAuth0 } from "@auth0/auth0-react";
+import {auth} from "express-oauth2-jwt-bearer"
 dotenv.config();
-
 const app: express.Application = express();
 app.use(bodyParser.json());
 // Enable CORS for all domains
 app.use(cors());
+
+//for oauth
+const jwtCheck = auth({
+    audience: 'This is a unique Identifier',
+    issuerBaseURL: 'https://dev-cd616eaxtu7so5dm.us.auth0.com/',
+    tokenSigningAlg: 'RS256'
+});
 
 const port: number = parseInt(process.env.PORT || '3001', 10);
 const host : string = process.env.HOST || 'localhost';
@@ -27,8 +35,8 @@ mongoose.connection.on("disconnected", () => {
 mongoose.connection.on("error", (err: Error) => {
   console.log(`mongoose Error: ${err}`);
 });
-
-app.use("/user", userRouter);
+app.use(jwtCheck)
+app.use("/user", userRoutes);
 app.listen(port, host, () => {
     console.log(`Backend is running on PORT : ${port}`);
   });
