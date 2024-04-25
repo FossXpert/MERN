39feb5143f961bc6015d3c4211a8a54dd82e999f
FossXpert <FossXpert@outlook.com>
2024-04-25 10:56:12 +0530

    "Add conditional rendering for SignIn/SignOut buttons based on authentication status"
    This commit includes the following changes:
    
    * Add a conditional rendering for the SignIn and SignOut buttons based on the authentication status of the user

--- 100x/yimmy/frontend/src/views/Home.tsx
+++ 100x/yimmy/frontend/src/views/Home.tsx
@@ -2,12 +2,18 @@ import { Button } from "@mui/material";
 import { useAuth0 } from "@auth0/auth0-react";
 export default function Home(){
     const {loginWithRedirect,isAuthenticated,user,logout} = useAuth0();
+
+    console.log("isAuthenticated",isAuthenticated)
     console.log(user)
     return (
         <div>
             <h1>Home</h1>
-            <p><Button onClick={()=>loginWithRedirect()}>SignIn Here</Button></p>
-            <p><Button onClick={()=>logout()}>SignOut Here</Button></p>
+            {isAuthenticated===false?
+            (<p><Button onClick={()=>loginWithRedirect()}>SignIn Here</Button></p>):
+            (<p><Button onClick={()=>logout()}>SignOut Here</Button></p>)
+            }
+            
+            
         </div>
     )
 }
