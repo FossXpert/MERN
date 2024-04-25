3486ef3f01d79ba240054bf883446fbc0b80b684
FossXpert <FossXpert@outlook.com>
2024-04-25 10:37:32 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/views/Home.tsx

--- 100x/yimmy/frontend/src/views/Home.tsx
+++ 100x/yimmy/frontend/src/views/Home.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import { useAuth0 } from "@auth0/auth0-react";
 export default function Home(){
     const {loginWithRedirect,isAuthenticated,user,logout} = useAuth0();
+    console.log(user)
     return (
         <div>
             <h1>Home</h1>
