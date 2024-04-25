c96fcdf6c7453334b943ef191ed5ddbff4252297
FossXpert <FossXpert@outlook.com>
2024-04-25 13:17:46 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/views/Home.tsx

--- 100x/yimmy/frontend/src/views/Home.tsx
+++ 100x/yimmy/frontend/src/views/Home.tsx
@@ -2,26 +2,24 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LogIn from "../components/Login";
 import Logout from "../components/Logout";
 import Profile from "../components/Profile";
-import { List } from "@mui/material";
+
 
 export default function Home(){
-    const {isAuthenticated,user} = useAuth0();
+    const {isAuthenticated,user,error,isLoading} = useAuth0();
     console.log("isAuthenticated",isAuthenticated)
     console.log(user)
     return (
-        <div>
-            <h1>Home</h1>
-        <div className="column">
-            {isAuthenticated===false?
-            (<LogIn/>):
-            (<Logout/>)
-            }
-        </div>
-        {isAuthenticated && 
-            <div className="column">
-                <List>Welcome {<Profile/>}</List>
-            </div>
-        }
-        </div>
+        <main className="column">
+            <h1>Auth0 Login</h1>
+            {error && <p>Authentication Error Mate</p>}
+            {!error && isLoading && <p> Loading...</p>}
+            {!error && !isLoading && (
+                <>
+                    <LogIn/>
+                    <Logout/>
+                    <Profile/>
+                </>
+            )}
+        </main>
     )
 }
