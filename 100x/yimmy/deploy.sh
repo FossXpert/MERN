820aaba74cebeb0579ad4c7ef081093a622e84ca
FossXpert <FossXpert@outlook.com>
2024-05-01 21:40:17 +0530

    Update deploy.sh script and add commands to start the application using pm2

--- 100x/yimmy/deploy.sh
+++ 100x/yimmy/deploy.sh
@@ -1,1 +1,13 @@
 ssh -t -i C:/Users/rayra/Documents/key-pair/yimmy-rahul-pc.pem ubuntu@ec2-15-207-19-241.ap-south-1.compute.amazonaws.com "sudo bash ~/deploy.sh"
+
+ubuntu deploy.sh file command is this : 
+--------------------------------------------------------------
+#!/bin/bash
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.12.2/bin
+
+cd home/MERN/100x/yimmy/backend/dist/src
+ git pull origin daily-changes
+ pm2 kill
+ pm2 start index.js
+
+---------------------------------------------------------------
