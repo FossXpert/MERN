50371f224dd7c8c79d5408ca84caae738ea68627
FossXpert <FossXpert@outlook.com>
2024-05-03 17:55:39 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/components/AppBar.tsx

--- 100x/yimmy/frontend/src/components/AppBar.tsx
+++ 100x/yimmy/frontend/src/components/AppBar.tsx
@@ -4,13 +4,21 @@ const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export default function ResponsiveAppBar(){
+
+    const isLoggedIn = true;
     return (
         <AppBar position='fixed'>
-            <Toolbar>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                {/* why componnet ="div" https://sprl.in/s3UmCMi */}
                 <Typography variant='h6' component="div" sx={{textAlign:'left', marginTop:'10px'}}>
                     Udemy
                 </Typography>
-                <Button color="inherit">Login</Button>
+                <Typography sx={{textAlign:'left', marginTop:'10px'}} >
+                   {!isLoggedIn && (<Button color="inherit">Login</Button>)}
+                   {isLoggedIn && (<Button color="inherit">Signup</Button>)}
+                                        
+                </Typography>
+                
             </Toolbar>
         </AppBar>
     )
