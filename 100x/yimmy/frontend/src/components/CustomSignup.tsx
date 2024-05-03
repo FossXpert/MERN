038e45f059661d52a952921dacf78cf8eee242a9
FossXpert <FossXpert@outlook.com>
2024-05-03 07:54:43 +0530

    Auto commit: Updated files: 100x/yimmy/backend/src/models/Course.ts

---
+++ 100x/yimmy/frontend/src/components/CustomSignup.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+export function CustomSignup(){
+
+    return (
+        <>
+        <Typography component='h1' variant="h5">Sign Up</Typography>
+        </>
+    )
+}
