cd52c36010c60a5bdd78443c169d626530465aef
FossXpert <FossXpert@outlook.com>
2024-05-03 18:08:37 +0530

    c

--- 100x/yimmy/frontend/src/components/AppBar.tsx
+++ 100x/yimmy/frontend/src/components/AppBar.tsx
@@ -1,11 +1,14 @@
 import { Button, Toolbar, Typography } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
+import { useRecoilValue } from 'recoil';
+import { atomUserName } from './atom';
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export default function ResponsiveAppBar(){
 
     const isLoggedIn = true;
+    const username = useRecoilValue(atomUserName);
     return (
         <AppBar position='fixed'>
             <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
@@ -16,8 +19,8 @@ export default function ResponsiveAppBar(){
                 <Typography sx={{textAlign:'left', marginTop:'10px'}} >
                    {!isLoggedIn && (<Button color="inherit">Login</Button>)}
                    {!isLoggedIn && (<Button color="inherit">Signup</Button>)}
-                   {isLoggedIn && (<Button color="inherit">atomUserName</Button>)}
-                   {!isLoggedIn && (<Button color="inherit">Login</Button>)}                 
+                   {isLoggedIn && (<Button color="inherit">{username}</Button>)}
+                   {isLoggedIn && (<Button color="inherit">Logout</Button>)}                 
                 </Typography>
                 
             </Toolbar>
