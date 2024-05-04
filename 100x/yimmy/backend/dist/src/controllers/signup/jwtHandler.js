5f5a7f54266baaf0839f01c3b4742cc885862600
FossXpert <FossXpert@outlook.com>
2024-05-04 23:35:30 +0530

    Auto commit: Updated files: 100x/yimmy/backend/dist/src/controllers/signup/jwtHandler.js, 100x/yimmy/backend/src/controllers/signup/jwtHandler.ts

--- 100x/yimmy/backend/dist/src/controllers/signup/jwtHandler.js
+++ 100x/yimmy/backend/dist/src/controllers/signup/jwtHandler.js
@@ -8,7 +8,7 @@ const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 require("dotenv").config();
 const secretKey = "Sec3t";
 const options = {
-    expiresIn: "10h",
+    expiresIn: "10s",
 };
 const createPayload = (userData) => {
     try {
