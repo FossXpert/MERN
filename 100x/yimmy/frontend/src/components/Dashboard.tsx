b148ce366cbddc1d6021c5dcc625ae255b454037
FossXpert <FossXpert@outlook.com>
2024-05-05 20:45:30 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/components/Dashboard.tsx

--- 100x/yimmy/frontend/src/components/Dashboard.tsx
+++ 100x/yimmy/frontend/src/components/Dashboard.tsx
@@ -1,11 +1,20 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import ResponsiveAppBar from "./AppBar";
 import CourseCard from "./CourseCard";
 
 export default function Dashboard(){
     return (
     <>
-        <ResponsiveAppBar/>
+    <ResponsiveAppBar/>
+    <Container maxWidth='lg'>
+        <Typography variant="h4" align="center" style={{marginTop:"50px"}}>
+            All Courses
+        </Typography>
+        <Grid container spacing={5} style={{marginTop:'20px'}}>
+        {<CourseCard/>}{<CourseCard/>}
+        {<CourseCard/>}{<CourseCard/>}
+        </Grid>
+    </Container>
     </>        
     )
 }
