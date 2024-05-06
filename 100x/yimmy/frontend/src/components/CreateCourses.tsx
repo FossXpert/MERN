20221a2187eedb8a6430476ed29077864c326bd6
FossXpert <FossXpert@outlook.com>
2024-05-06 13:22:41 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/components/CreateCourses.tsx

--- 100x/yimmy/frontend/src/components/CreateCourses.tsx
+++ 100x/yimmy/frontend/src/components/CreateCourses.tsx
@@ -1,19 +1,16 @@
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 
 const CreateCourses = () => {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-        {<h3>Create Courses Here</h3>}
+      <Grid item xs={6} md={6}>
         <TextField
           fullWidth
           id="course-title"
           label="Course Title"
           variant="outlined"
         />
-        {/* Description */}
         <TextField
           fullWidth
           id="course-description"
@@ -23,45 +20,6 @@ const CreateCourses = () => {
           variant="outlined"
           style={{ marginTop: '1rem' }}
         />
-      </Grid>
-
-      {/* Second half */}
-      <Grid item xs={12} md={6}>
-        {/* Instructor */}
-        <TextField
-          fullWidth
-          id="course-instructor"
-          label="Instructor"
-          variant="outlined"
-        />
-        {/* Duration */}
-        <TextField
-          fullWidth
-          id="course-duration"
-          label="Duration"
-          variant="outlined"
-          style={{ marginTop: '1rem' }}
-        />
-        {/* Price */}
-        <TextField
-          fullWidth
-          id="course-price"
-          label="Price"
-          variant="outlined"
-          style={{ marginTop: '1rem' }}
-        />
-        {/* Difficulty Level */}
-        <TextField
-          fullWidth
-          id="course-difficulty"
-          label="Difficulty Level"
-          variant="outlined"
-          style={{ marginTop: '1rem' }}
-        />
-        {/* Submit Button */}
-        <Button fullWidth variant="contained" color="primary" style={{ marginTop: '1rem' }}>
-          Create Course
-        </Button>
       </Grid>
     </Grid>
   );
