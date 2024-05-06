40d83d2b6e09500f3f75187737f9759889b97366
FossXpert <FossXpert@outlook.com>
2024-05-06 13:42:44 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/components/CreateCourses.tsx

--- 100x/yimmy/frontend/src/components/CreateCourses.tsx
+++ 100x/yimmy/frontend/src/components/CreateCourses.tsx
@@ -21,6 +21,23 @@ const CreateCourses = () => {
           style={{ marginTop: '1rem' }}
         />
       </Grid>
+      <Grid item xs={6} md={6}>
+      <TextField
+          fullWidth
+          id="course-title"
+          label="Course Title"
+          variant="outlined"
+        />
+        <TextField
+          fullWidth
+          id="course-description"
+          label="Description"
+          multiline
+          rows={4}
+          variant="outlined"
+          style={{ marginTop: '1rem' }}
+        />
+      </Grid>
     </Grid>
   );
 };
