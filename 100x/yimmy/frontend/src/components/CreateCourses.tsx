7272129c1e35d31bc547a93fcd439aa31bfd9a6c
FossXpert <FossXpert@outlook.com>
2024-05-07 15:47:34 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/components/CreateCourses.tsx

--- 100x/yimmy/frontend/src/components/CreateCourses.tsx
+++ 100x/yimmy/frontend/src/components/CreateCourses.tsx
@@ -1,40 +1,37 @@
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
+import React from 'react';
+import { Grid, TextField, Button, Select, MenuItem } from '@mui/material';
 import { useState } from 'react';
 import CourseCard from './CourseCard';
-import { Button } from '@mui/material';
 import { useRecoilState } from 'recoil';
-import {v4 as uuid4} from 'uuid'
+import { v4 as uuid4 } from 'uuid';
 import axios from 'axios';
 import { atomAdminId, atomCategory, atomCourseDescription, atomCoursePrice, atomCourseTitle, atomImageLink, decodePayload, decodePayloadInterface, getToken } from './atom';
 import { BASE_URL1 } from '../config';
 import { zodCourseDetail } from '@rahulray8518/common';
+
 const CreateCourses = () => {
+  const [isCourseCreated, setIsCourseCreated] = useState(false);
+  const [title, setTitle]: [string, (title: string) => void] = useRecoilState(atomCourseTitle);
+  const [description, setDescription]: [string, (description: string) => void] = useRecoilState(atomCourseDescription);
+  const [price, setPrice]: [number, (price: number) => void] = useRecoilState(atomCoursePrice);
+  const [category, setCategory]: [string, (category: string) => void] = useRecoilState(atomCategory);
+  const [admin_id, setAdminId]: [string, (admin_id: string) => void] = useRecoilState(atomAdminId);
+  const [courseId, setCourseId]: [string, (courseId: string) => void] = useState("");
+  const [imageLink, setImageLink]: [string, (imageLink: string) => void] = useRecoilState(atomImageLink);
 
-    const [isCourseCreated,setIsCourseCreated] = useState(false);
-    const [title,setTitle]:[string,(title:string) => void] = useRecoilState(atomCourseTitle);
-    const [description,setDescription]:[string,(description:string) => void]= useRecoilState(atomCourseDescription);
-    const [price,setPrice]:[number,(price:number) => void]= useRecoilState(atomCoursePrice);
-    const [category,setCategory]:[string,(category:string) => void] = useRecoilState(atomCategory);
-    const [admin_id,setAdminId]:[string,(admin_id:string) => void] = useRecoilState(atomAdminId)
-    const [courseId,setCourseId]:[string,(courseId:string) => void] = useState("");
-    const [imageLink,setImageLink]:[string,(imageLink:string) => void] = useRecoilState(atomImageLink)
+  const payload: decodePayloadInterface | undefined = decodePayload(getToken());
 
-    const payload : decodePayloadInterface | undefined = decodePayload(getToken());
+  const handleCreateButton = () => {
+    setIsCourseCreated(true);
+    setCourseId(uuid4());
+    payload ? setAdminId(payload.id) : null;
 
-    const handleCreateButton = () => {
-        setIsCourseCreated(true);
-        setCourseId(uuid4());
-        payload?setAdminId(payload.id) : null;
-    
-        const courseBody : zodCourseDetail = {
-            title,description,price,category,admin_id,courseId,imageLink
-        }
-        
-        const response = axios.post(`${BASE_URL1}/${payload?.role}/createcourse`,{
-            
-        })
+    const courseBody: zodCourseDetail = {
+      title, description, price, category, admin_id, courseId, imageLink
     }
+
+    const response = axios.post(`${BASE_URL1}/${payload?.role}/createcourse`, courseBody);
+  }
 
   return (
     <Grid container spacing={2}>
@@ -44,7 +41,7 @@ const CreateCourses = () => {
           id="course-title"
           label="Course Title"
           variant="outlined"
-          onChange={(e : React.ChangeEvent<HTMLInputElement> )=> {setTitle(e.target.value)}}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setTitle(e.target.value) }}
         />
         <TextField
           fullWidth
@@ -54,35 +51,34 @@ const CreateCourses = () => {
           rows={4}
           variant="outlined"
           style={{ marginTop: '1rem' }}
-          onChange={(e : React.ChangeEvent<HTMLInputElement>) => { setDescription(e.target.value)}}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => { setDescription(e.target.value) }}
         />
         <TextField
           fullWidth
           id="price"
           label="Course Price"
           variant="outlined"
-          onChange={(e:React.ChangeEvent<HTMLInputElement>) => {
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
             const priceValue = parseFloat(e.target.value)
-            setPrice(priceValue)}}
+            setPrice(priceValue)
+          }}
         />
-        <TextField
-          fullWidth
-          id="Category"
-          label="Course Title"
-          variant="outlined"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setCategory(e.target.value)}}
-        />
-        <TextField
+        <Select
           fullWidth
           id="Category"
           label="Category"
           variant="outlined"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setCategory(e.target.value)}}
-        />
+          value={category}
+          onChange={(e: React.ChangeEvent<{ value: unknown }>) => { setCategory(e.target.value as string) }}
+        >
+          <MenuItem value="code">Code</MenuItem>
+          <MenuItem value="design">Design</MenuItem>
+          <MenuItem value="architect">Architect</MenuItem>
+        </Select>
         <Button onClick={handleCreateButton}>Create</Button>
       </Grid>
       <Grid item xs={6} md={6}>
-        {isCourseCreated && <CourseCard/>}
+        {isCourseCreated && <CourseCard />}
       </Grid>
     </Grid>
   );
