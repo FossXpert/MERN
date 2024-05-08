8e6fe2e29e081bf3b5dbdb29b597daeb434bdc46
FossXpert <FossXpert@outlook.com>
2024-05-08 11:48:01 +0530

    Auto commit: Updated files: 100x/yimmy/frontend/src/components/CustomHooks.tsx

--- 100x/yimmy/frontend/src/components/CustomHooks.tsx
+++ 100x/yimmy/frontend/src/components/CustomHooks.tsx
@@ -1,28 +1,35 @@
 import { useEffect, useState } from "react"
 import { BACKEND_BASE_URL, BASE_URL } from "../config";
 import axios from "axios";
+import { zodCourseDetail } from "@rahulray8518/common";
 
 const useCourseManagementHook = () => {
-    const [courses,setCourses] = useState([]);
+    const [courses,setCourses]  = useState([]);
     const [loading,setLoading] = useState(true);
 
     useEffect(()=>{
-        const fetchCourses = async() => {
-            try {
-                const response = await axios.get(`${BACKEND_BASE_URL}/admin/getcourses`);
-                setCourses(response.data);
-                setLoading(false)
-            } catch (error) {
-                console.error("Error Fetching Courses",error);
-                setLoading(false)
-            }
-        };
         fetchCourses();
     },[]);
 
+    const fetchCourses = async() => {
+        try {
+            const response = await axios.get(`${BACKEND_BASE_URL}/admin/getcourses`);
+            setCourses(response.data);
+            setLoading(false)
+        } catch (error) {
+            console.error("Error Fetching Courses",error);
+            setLoading(false)
+        }
+    };
 
-    const createCourse = async() => {
-        
+    const createCourse = async(newCourse : zodCourseDetail) => {
+        try{
+            const response = axios.post(`${BACKEND_BASE_URL}/admin/createcourse`,newCourse);
+            setCourses([...courses , response.data]);
+        }catch(error){
+            console.error("Error Creating Courses")
+            throw error;
+        }
     }
 
 
