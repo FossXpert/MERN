b930c4a53f5c29129c4650b39abce66210e30860
fossxpert <fossxpert@outlook.com>
2024-05-14 11:18:01 +0000

    chore: Remove unnecessary files and dependencies

---
+++ 100x/udemy-pro/backend/src/index.ts
@@ -0,0 +1,25 @@
+require('dotenv').config();
+import { app } from "./app";
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+
+app.use(express.json({limit:'50mb'}));
+app.use(cookieParser());
+app.use(cors());
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
+
+app.get('/',(req:Request,res:Response)=>{
+    res.status(200).json({
+        message : "Valid Route",
+    })
+})
+
+app.get('*',(req:Request,res:Response)=>{
+    res.status(404).json({
+        message : "Invalid Route",
+    })
+})
