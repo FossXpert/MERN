ed3e95d341d23fba5b985230e10b085c9aadee79
fossxpert <fossxpert@outlook.com>
2024-05-15 09:05:38 +0000

    refactor: Update .env file and index.ts to include new environment variables and connect to the database

--- 100x/udemy-pro/backend/src/index.ts
+++ 100x/udemy-pro/backend/src/index.ts
@@ -1,9 +1,10 @@
 require('dotenv').config();
 import { app } from "./app";
 import cors from 'cors';
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import connectDB from "./utills/db";
+import errorMiddleware from "./middleware/error";
 
 app.use(express.json({limit:'50mb'}));
 app.use(cookieParser());
@@ -14,14 +15,16 @@ app.listen(process.env.PORT,()=>{
     connectDB();
 })
 
-app.get('/',(req:Request,res:Response)=>{
+app.get('/',(req:Request,res:Response,next:NextFunction)=>{
     res.status(200).json({
         message : "Valid Route",
     })
 })
 
-app.get('*',(req:Request,res:Response)=>{
+app.all('*',(req:Request,res:Response,next:NextFunction)=>{
     res.status(404).json({
         message : "Invalid Route",
     })
 })
+
+app.use(errorMiddleware);
---
+++ 100x/udemy-pro/backend/src/middleware/catchAsyncError.ts
@@ -0,0 +1,11 @@
+import { Request, Response,NextFunction } from "express";
+
+export const catchAsyncError = 
+(theFunc : any) => async(req:Request,res:Response,next:NextFunction) => {
+    Promise.resolve(theFunc(req,res,next)).catch(next);
+};
+
+
+
+
+
