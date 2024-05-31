dff1e0d14cb9a05929e62ab905bf5958b4a67a68
FossXpert <FossXpert@outlook.com>
2024-05-31 10:20:44 +0000

    feat: Add scroll event listener to update header styling

--- 100x/udemy-pro/frontend/app/components/Header.tsx
+++ 100x/udemy-pro/frontend/app/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link';
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 type Props = {
     open: boolean;
@@ -11,13 +11,26 @@ type Props = {
 const Header: FC<Props> = (props) => {
     const [active, setActive] = useState(false);
     const [openSidebar, setOpenSidebar] = useState(false);
+
+    if(typeof window !== "undefined"){
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY > 80){
+                setActive(true)
+            }else{
+                setActive(false)
+            }
+        })
+    };
+    console.log(active)
     return (
         <div className='w-full relative'>
-            <div className={`${active ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500" : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"}`}>
+            <div className={`${
+                active ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
+                        : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"}`}>
                 <div className='w-[95%] 800px:w-[92%] m-auto py-2 h-full'>
                     <div className='w-full h-[80px] flex items-center justify-between p-3'>
                         <div>
-                            <Link href={'/'} className={`text-[25px] font-popping font-[500] text-black dark:text-white`}>
+                            <Link href={'/'} className={`text-[25px] font-Popping font-[500] text-black dark:text-white`}>
                                 ELearning
                             </Link>
                         </div>
