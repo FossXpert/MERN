cc5b2d803fd6257d325df95c42358be44f90259b
FossXpert <FossXpert@outlook.com>
2024-05-31 09:32:34 +0000

    feat: Add lazy loading for images and update font styles

--- 100x/udemy-pro/frontend/app/page.tsx
+++ 100x/udemy-pro/frontend/app/page.tsx
@@ -1,12 +1,15 @@
 'use client'; // Writing this means the component wil be rendered on client Side
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Heading from './utills/Heading';
+import Header from './components/Header';
 
 interface Props{
 
 }
 
 const Page : FC<Props> = (props)=>{
+  const [open,setOpen] = useState(false);
+  const [activeItem,setActiveItems] = useState(0);
 
   return(
     <div>
@@ -14,6 +17,7 @@ const Page : FC<Props> = (props)=>{
       title='E-Learning' 
       description='E-Learning is a platform for students to learn and get help from teachers'
       keywords='Programming,MERN,Redux,Machine Learning, AI'/>
+      <Header open={open} activeItem={activeItem} setOpen={setOpen}/>
     </div>
   )
 };
