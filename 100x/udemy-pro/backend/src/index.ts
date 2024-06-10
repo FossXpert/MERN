74cf2fba7c450be4eb9753a5be4a985e5e3b0f78
FossXpert <FossXpert@outlook.com>
2024-06-10 16:30:08 +0530

    Auto commit: Updated files: 100x/udemy-pro/backend/package-lock.json, 100x/udemy-pro/backend/src/index.ts, 100x/udemy-pro/frontend/.env

--- 100x/udemy-pro/backend/src/index.ts
+++ 100x/udemy-pro/backend/src/index.ts
@@ -17,10 +17,11 @@ app.use(express.json({limit:'50mb'}));
 app.use(cookieParser());
 app.use(
     cors({
-        origin : ["http://35.226.62.251:3000"],
+        origin : ["http://localhost:3000"],
         credentials : true,
     })
 );
+
 
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
