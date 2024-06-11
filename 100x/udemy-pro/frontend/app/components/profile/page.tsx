a51fe586e2c49d6f4c361633fd05e58810482ec8
FossXpert <FossXpert@outlook.com>
2024-06-11 13:19:06 +0530

    chore: Update profile page with heading and header components

--- 100x/udemy-pro/frontend/app/components/profile/page.tsx
+++ 100x/udemy-pro/frontend/app/components/profile/page.tsx
@@ -1,9 +1,24 @@
-import React, { FC } from 'react'
-
+import Heading from '../../utills/Heading'
+import Protected from '../hooks/useProtected'
+import React, { FC, useState } from 'react'
+import Header from '../../components/Header'
 type Props = {}
 
-const page: FC<Props> = (props) => {
+const page: FC<Props> = (props:Props) => {
+
+    const [open,setOpen] = useState(false);
+  const [activeItem,setActiveItems] = useState(0);
+  const [route,setRoute] = useState('signin')
   return (
-    <div>page</div>
+    <div>
+        <Protected>
+        <Heading
+      title='E-Learning' 
+      description='E-Learning is a platform for students to learn and get help from teachers'
+      keywords='Programming,MERN,Redux,Machine Learning, AI'/>
+      <Header open={open} activeItem={activeItem} setOpen={setOpen} route={route} setRoute={setRoute}/>
+        </Protected>
+    </div>
   )
 }
+
