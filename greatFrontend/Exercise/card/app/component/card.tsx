bb41cb8e92b22991daecc19fdbb05bb1fe11dd20
Rahul Ray <FossXpert@outlook.com>
2024-06-25 23:38:29 +0000

    hhh
    nn

---
+++ greatFrontend/Exercise/card/app/component/card.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react"
+
+type Props =  {
+
+}
+
+
+export const Card : FC<Props> = () => {
+    return (
+        <>
+        <div className="flex mt-[50px] ">
+                hi
+        </div>
+        </>
+    )
+}
