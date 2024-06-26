416eb9ca7b3390470e9a425e70bd5ee1b3c51081
Rahul Ray <FossXpert@outlook.com>
2024-06-26 13:28:45 +0000

    Complete Card App

--- greatFrontend/Exercise/card/app/component/card.tsx
+++ greatFrontend/Exercise/card/app/component/card.tsx
@@ -1,4 +1,9 @@
-import { FC } from "react"
+import { FC } from "react";
+import styles from './ReactComponent.module.css';
+import Image from "next/image";
+import thumbnail from '../assets/thumnail.png'
+
+
 
 type Props =  {
 
@@ -8,8 +13,27 @@ type Props =  {
 export const Card : FC<Props> = () => {
     return (
         <>
-        <div className="flex mt-[50px] ">
-                hi
+        <div className={styles.container}>
+        <div className={styles.box}>
+        <div className={styles.cardtop}>
+            <div className={styles.thumbnailDiv}>
+            <Image height={30} width={32} src={thumbnail} alt="" className={styles.image}/>
+            </div>
+            <div className={styles.h1}>
+                <div className={styles.h11}>
+                <h1>Sarah Dole</h1>
+                </div>
+            <div className={styles.smallH1}>
+                @sarahdole
+            </div>
+            </div>
+        </div>
+        <div className={styles.cardbottom}>
+             I've been searching for high-quality abstract images for
+             my design projects, and I'm thrilled to have found this platform. 
+             The variety and depth of creativity are astounding!
+        </div>
+        </div>
         </div>
         </>
     )
