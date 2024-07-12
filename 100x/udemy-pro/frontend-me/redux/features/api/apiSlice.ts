111fc131eb5ad9654cb9ca0e199a929a132eb76e
FossXpert <FossXpert@outlook.com>
2024-07-12 13:05:47 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/redux/features/api/apiSlice.ts

--- 100x/udemy-pro/frontend-me/redux/features/api/apiSlice.ts
+++ 100x/udemy-pro/frontend-me/redux/features/api/apiSlice.ts
@@ -2,6 +2,12 @@
 
 require('dotenv').config();
 
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
-import { userLoggedIn } from "../auth/authSlice";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
+const apiSlice = createApi({
+    reducerPath : 'api',
+    baseQuery : fetchBaseQuery({
+        baseUrl : process.env.NEXT_PUBLIC_SERVER_URI
+    }),
+    
+})
