4014c00b7e0ffa84e82d3bbab5f7fc300f76a7eb
FossXpert <FossXpert@outlook.com>
2024-07-22 17:45:44 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend2/app/page.tsx

---
+++ 100x/udemy-pro/frontend2/app/redux/features/auth/authApi.ts
@@ -0,0 +1,62 @@
+import { apiSlice } from "../api/apiSlice";
+import { userLoggedIn, userRegistration } from "./authSlice";
+
+export const authApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: ({email, password}) => ({
+                url: 'user/login',
+                method: 'POST',
+                body: {
+                    email, password
+                },
+                credentials: 'include' as const,
+            }),
+            async onQueryStarted(args,{dispatch,queryFulfilled}){
+                try {
+                    const {data} = await queryFulfilled;
+                    userLoggedIn({
+                        accessToken : data.accessToken,
+                        user : data.user
+                    })
+                } catch (error) {
+                    
+                }
+            }
+        }),
+        signup: builder.mutation({
+            query : ({name,email,password}) => ({
+                url : 'user/registration',
+                method : 'POST',
+                body:{
+                    name,email,password
+                },
+                credentials : 'include' as const
+            }),
+            async onQueryStarted({otp,authToken},{dispatch,queryFulfilled}){
+                try {
+                    const {data} = await queryFulfilled;
+                    dispatch(userRegistration({token : data.activationToken}));
+                    console.log("Inside RTK Query Signup: Data is equal to ",data.activationToken)
+                } catch (error) {
+                    console.log("Inside error");
+                    throw error;
+                }
+            }
+        }),
+        verification : builder.mutation({
+            query : ({otp,authToken}) => ({
+                url : 'user/verify',
+                method : 'POST',
+                body : {
+                    authentication_token : authToken,
+                    authentication_code : otp
+                },
+                credentials : 'include' as const
+            }),
+        }),
+    })
+
+})
+
+export const  {useLoginMutation,useSignupMutation,useVerificationMutation} = authApi;
