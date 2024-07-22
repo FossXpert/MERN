777c2574e404e4aca37fe4680f0a5930a15a8491
FossXpert <FossXpert@outlook.com>
2024-07-22 12:07:28 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Profile/SideBarProfile.tsx, 100x/udemy-pro/frontend-me/app/css/profile.css

---
+++ 100x/udemy-pro/frontend-me/app/components/Profile/SideBarProfile.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../css/SideBarProfile'
+type Props = {}
+
+const SideBarProfile = (props: Props) => {
+  return (
+    <>
+    <div className='container'>
+    Hi
+    </div>
+    </>
+  )
+}
+
+export default SideBarProfile
