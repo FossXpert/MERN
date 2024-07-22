0866707352bc98da8bc1568a75e4b09bad9bd1de
FossXpert <FossXpert@outlook.com>
2024-07-22 12:27:43 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Profile/SideBarProfile.tsx

--- 100x/udemy-pro/frontend-me/app/components/Profile/SideBarProfile.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Profile/SideBarProfile.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import '../../css/sidebarProfile.css'
+import { useSelector } from 'react-redux'
+import useScreenSize from '@/redux/features/screenSize/hook/useScreenSize'
 type Props = {}
 
 const SideBarProfile = (props: Props) => {
+    useScreenSize();
+    const {isMobile,sSize} = useSelector((state:any)=>state.screen);
   return (
     <>
+    Screen Size : `${sSize}`
     <div className='container'>
         <div className='box'>
         </div>
