8f18b25b395b46cb38709b35ec171a7b3d66406c
FossXpert <FossXpert@outlook.com>
2024-07-22 16:29:30 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/redux/Providers.ts

--- 100x/udemy-pro/frontend-me/redux/Providers.ts
+++ 100x/udemy-pro/frontend-me/redux/Providers.ts
@@ -1,2 +1,11 @@
-import { Provider } from "react-redux";
+import React,{ReactNode} from 'react'
+import { Provider } from 'react-redux'
+import {store} from "./store"
 
+interface ProviderProps {
+    children: ReactNode
+}
+
+export function Providers({children}:ProviderProps){
+    return <Provider store={store}>{children}</Provider>
+}
