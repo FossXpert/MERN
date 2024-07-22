276894ba35e8ea477750883d0ac89695acd8b489
FossXpert <FossXpert@outlook.com>
2024-07-22 11:42:17 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Profile/Profile.tsx

--- 100x/udemy-pro/frontend-me/app/components/Profile/Profile.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Profile/Profile.tsx
@@ -1,33 +1,15 @@
 import React, { FC } from 'react'
 import '../../css/profile.css'
-import { Drawer, makeStyles, Typography } from '@mui/material';
 
 type Props = {
 }
 
-const useStyles = makeStyles({
-  page : {
-    background : '#f9f9f9',
-    width : '100%'
-  }
-})
-
 const Profile:FC<Props> = ({}) => {
-
-  const classes = useStyles();''
 
   return (
     <>
-    <div className="container">
-        <Drawer
-        className={classes}
-        >
-          <div>
-            <Typography variant='h5'>
-                Hello World
-            </Typography>
-          </div>
-        </Drawer>
+    <div className="">
+        
     </div>
     </>
   )
