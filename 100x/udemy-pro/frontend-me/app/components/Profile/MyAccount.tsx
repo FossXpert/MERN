27bcc691ba31bc88e062483a68b253821af0f2df
FossXpert <FossXpert@outlook.com>
2024-07-23 17:21:16 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Profile/MyAccount.tsx

--- 100x/udemy-pro/frontend-me/app/components/Profile/MyAccount.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Profile/MyAccount.tsx
@@ -1,11 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 type Props = {}
 
 const MyAccount = (props: Props) => {
+    const {user} = useSelector((state:any) => state.auth)
   return (
-    <div>
-        
+    <div className='myaccount-container'>
+        <Image src={user?.avatar?.url} alt='No'/>
     </div>
   )
 }
