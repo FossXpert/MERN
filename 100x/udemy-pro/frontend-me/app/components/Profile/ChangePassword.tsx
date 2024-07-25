4b6655dc22eac21624cf9f571ada5e2b2ff86548
FossXpert <FossXpert@outlook.com>
2024-07-25 11:52:30 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Profile/ChangePassword.tsx, 100x/udemy-pro/frontend-me/app/css/css-profile/myaccount.css

--- 100x/udemy-pro/frontend-me/app/components/Profile/ChangePassword.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Profile/ChangePassword.tsx
@@ -2,55 +2,62 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import '../../css/css-profile/changePassword.css'
+import '../../css/css-profile/myaccount.css'
+
 
 type Props = {}
 
 const ChangePassword = (props: Props) => {
-    const {user} = useSelector((state:any) => state.auth);
-    const [oldPassword,setOldPassword] = useState('');
-    const [newPassword,setNewPassword] = useState('');
-    const [confirmPassword,setConfirmPassword] = useState('');
+    const { user } = useSelector((state: any) => state.auth);
+    const [oldPassword, setOldPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
 
-  return (
-    <>
-    <div className="password-container">
-        <div className="image-container">
-            <Image
-             className='myaccount-image' 
-             src={user?.avatar?.url} 
-             width={64} height={64} alt='No'/>
-        </div>
-        <p>Change Password</p>
-        <div className='myaccount-text'>
-            <label htmlFor="oldpass">Old Password</label>
-            <input type='password'
-            id='oldpass'
-            name='oldpass'
-            value={oldPassword}
-            placeholder='Old Password'
-            onChange={(e)=>setOldPassword(e.target.value)}
-            />
-            <label htmlFor="newpass">New Password</label>
-            <input type='password'
-            id='newpass'
-            name='newpass'
-            value={newPassword}
-            placeholder='New Password'
-            onChange={(e)=>setNewPassword(e.target.value)}
-            />
-            <label htmlFor="cpass">Confirm Password</label>
-            <input type='password'
-            id='cpass'
-            name='cpass'
-            value={confirmPassword}
-            placeholder='Confirm Password'
-            onChange={(e)=>setConfirmPassword(e.target.value)}
-            />
-        </div>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <div className="password-container">
+                <div className="image-container">
+                    <Image
+                        className='myaccount-image'
+                        src={user?.avatar?.url}
+                        width={64} height={64} alt='No' />
+                </div>
+                <p>Change Password</p>
+                <div className='myaccount-text'>
+                    <div className='myaccount-text-1'>
+                        <label htmlFor="oldpass">Old Password</label>
+                        <input type='password'
+                            id='oldpass'
+                            name='oldpass'
+                            value={oldPassword}
+                            placeholder='Old Password'
+                            onChange={(e) => setOldPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className='myaccount-text-1'>
+                        <label htmlFor="newpass">New Password</label>
+                        <input type='password'
+                            id='newpass'
+                            name='newpass'
+                            value={newPassword}
+                            placeholder='New Password'
+                            onChange={(e) => setNewPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className='myaccount-text-1'></div>
+                    <label htmlFor="cpass">Confirm Password</label>
+                    <input type='password'
+                        id='cpass'
+                        name='cpass'
+                        value={confirmPassword}
+                        placeholder='Confirm Password'
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default ChangePassword
