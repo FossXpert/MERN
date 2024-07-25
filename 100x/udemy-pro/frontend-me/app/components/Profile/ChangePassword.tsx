4ecf605fe25d9b1b1be65360a2e5de680a3bc5c3
FossXpert <FossXpert@outlook.com>
2024-07-25 06:36:44 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Profile/ChangePassword.tsx

--- 100x/udemy-pro/frontend-me/app/components/Profile/ChangePassword.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Profile/ChangePassword.tsx
@@ -1,11 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
+import { useSelector } from 'react-redux';
+import '../../css/css-profile/changePassword.css'
 
 type Props = {}
 
 const ChangePassword = (props: Props) => {
+    const {user} = useSelector((state:any) => state.auth);
   return (
     <>
     <div className="password-container">
+        <div className="image-container">
+            <Image
+             className='myaccount-image' 
+             src={user?.avatar?.url} 
+             width={64} height={64} alt='No'/>
+        </div>
+        <h4></h4>
         
     </div>
     </>
