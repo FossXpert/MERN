6da11971277c7a64c22128629c825ef8186543fe
FossXpert <FossXpert@outlook.com>
2024-07-25 17:01:34 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Profile/Profile.tsx

--- 100x/udemy-pro/frontend-me/app/components/Profile/Profile.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Profile/Profile.tsx
@@ -5,6 +5,7 @@ import MyAccount from './MyAccount'
 import EnrolledCourses from './EnrolledCourses'
 import ChangePassword from './ChangePassword'
 import AdminDashboard from './AdminDashboard'
+import Link from 'next/link'
 type Props = {}
 
 const Profile = (props: Props) => {
@@ -26,7 +27,9 @@ const Profile = (props: Props) => {
                 active === 3 && <ChangePassword/>
             }
             {
-                active === 4 && <AdminDashboard/>
+                active === 4 && (
+                    <Link href={'/'}>Admin Dashboard</Link>
+                )
             }
         </div>
     </div>
