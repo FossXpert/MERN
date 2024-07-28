6d759987196135b8431717b4adfdd7754e37a7e8
FossXpert <FossXpert@outlook.com>
2024-07-28 16:34:15 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Admin/Sidebar/AdminSidebar.tsx

--- 100x/udemy-pro/frontend-me/app/components/Admin/Sidebar/AdminSidebar.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Admin/Sidebar/AdminSidebar.tsx
@@ -1,124 +1,130 @@
-import React, { useState } from 'react';
 import Image from 'next/image';
-import profileImage from '../../../assets/thumnail.png';
-import '../../../css/css-admin/adminSidebar.css';
+import React, { useState } from 'react'
+import { JSX } from 'react';
+import profileImage from '../../../assets/thumnail.png'
+import '../../../css/css-admin/adminSidebar.css'
 import { MdDashboard } from 'react-icons/md';
 import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
-interface MenuItem {
+type Props = {}
+
+interface MenuItems{
     menuTitle: string;
-    menuIcon: JSX.Element;
-    link: string;
-    subMenu: SubMenuItem[];
-    active: boolean;
-    open: boolean;
-    dropDownIcon: JSX.Element;
+    menuIcon : JSX.Element;
+    link : string;
+    subMenu : SubMenuItems[];
+    active : boolean;
+    open : boolean;
+    drodDownIcon: JSX.Element;
 }
-
-interface SubMenuItem {
+interface SubMenuItems{
     subMenuTitle: string;
-    subMenuIcon: JSX.Element;
-    subMenuLink: string;
-    subMenuActive: boolean;
+    subMenuIcon : JSX.Element;
+    subMenuLink : string;
+    subMenuActive : boolean;
 }
+const AdminSidebar = (props: Props) => {
 
-const menuItems: MenuItem[] = [
-  {
-    menuTitle: 'Main',
-    menuIcon: <MdDashboard />,
-    link: '#',
-    subMenu: [],
-    active: true,
-    open: false,
-    dropDownIcon: <IoIosArrowDown />,
-  },
-  {
-    menuTitle: 'Audience',
-    menuIcon: <MdDashboard />,
-    link: '#',
-    subMenu: [
-      {
-        subMenuTitle: 'Users',
-        subMenuIcon: <MdDashboard />,
-        subMenuLink: '#',
-        subMenuActive: false,
-      },
-      {
-        subMenuTitle: 'Subscribers',
-        subMenuIcon: <MdDashboard />,
-        subMenuLink: '#',
-        subMenuActive: false,
-      },
-    ],
-    active: false,
-    open: false,
-    dropDownIcon: <IoIosArrowDown />,
-  },
-];
+    const [toggle,setToggle] = useState(false);
+    
+    const handleToggle = () =>{
+        setToggle(!toggle);
+    }
 
-const AdminSidebar: React.FC = () => {
-  const [menuState, setMenuState] = useState<MenuItem[]>(menuItems);
+    
 
-  const handleToggle = (index: number) => {
-    setMenuState((prevState) =>
-      prevState.map((item, idx) =>
-        idx === index ? { ...item, open: !item.open } : item
-      )
-    );
-  };
-
-  return (
-    <div className="ascontainer">
-      <div className="assidebar">
-        <div className="ashead">
-          <div className="asuser-image">
-            <Image width={30} src={profileImage} alt="No" />
+  return ( 
+    <>
+      <div className="ascontainer">
+        <div className="assidebar">
+          <div className="ashead">
+            <div className="asuser-image">
+              <Image width={30} src={profileImage} alt='No'/>
+            </div>
+            <div className="asuser-details">
+              <p className='astitle'>Web Developer</p>
+              <p className='asname'>Rahul Rai</p>
+            </div>
           </div>
-          <div className="asuser-details">
-            <p className="astitle">Web Developer</p>
-            <p className="asname">Rahul Rai</p>
-          </div>
-        </div>
-        <div className="asnav">
-          {menuState.map((menuItem, index) => (
-            <div className="asmenu" key={index}>
-              <p className="astitle">{menuItem.menuTitle}</p>
+          <div className="asnav">
+            <div className="asmenu">
+              <p className='astitle'>Main</p>
               <ul>
-                <li className={menuItem.active ? 'active' : ''}>
-                  <a href={menuItem.link}>
-                    {menuItem.menuIcon}
-                    <span className="as-span-text">{menuItem.menuTitle}</span>
-                    {menuItem.subMenu.length > 0 && (
-                      <span onClick={() => handleToggle(index)}>
-                        {menuItem.open ? (
-                          <IoIosArrowUp className="arrow" />
-                        ) : (
-                          <IoIosArrowDown className="arrow" />
-                        )}
-                      </span>
-                    )}
+                <li className='active'>
+                  <a href='#'>
+                    <MdDashboard className="icon"/>
+                    <span className='as-span-text'>Dashboard</span>
                   </a>
-                  {menuItem.subMenu.length > 0 && (
-                    <ul className={menuItem.open ? 'assub-menu' : 'disabled'}>
-                      {menuItem.subMenu.map((subMenuItem, subIndex) => (
-                        <li key={subIndex}>
-                          <a href={subMenuItem.subMenuLink}>
-                            {subMenuItem.subMenuIcon}
-                            <span className="as-span-text">
-                              {subMenuItem.subMenuTitle}
-                            </span>
-                          </a>
-                        </li>
-                      ))}
-                    </ul>
-                  )}
+                </li>
+                <li>
+                  <a href='#'>
+                    <MdDashboard className="icon"/>
+                    <span className='as-span-text'>Audience</span>
+                    { toggle? (<IoIosArrowUp className='arrow' onClick={handleToggle} />):
+                        (<IoIosArrowDown className='arrow' onClick={handleToggle} />)}
+                  </a>
+                  <ul className={toggle ? 'assub-menu' : 'disabled'}>
+                    <li>
+                      <a href='#'>
+                        <MdDashboard className="icon"/>
+                        <span className='as-span-text'>Users</span>
+                      </a>
+                    </li>
+                    <li> 
+                      <a href='#'>
+                        <MdDashboard className="icon"/>
+                        <span className='as-span-text'>Subscribers</span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li className=''>
+                  <a href='#'>
+                    <MdDashboard className="icon"/>
+                    <span className='as-span-text'>Post</span>
+                  </a>
+                </li>
+                <li className=''>
+                  <a href='#'>
+                    <MdDashboard className="icon"/>
+                    <span className='as-span-text'>Post</span>
+                  </a>
+                </li>
+                <li className=''>
+                  <a href='#'>
+                    <MdDashboard className="icon"/>
+                    <span className='as-span-text'>Income</span>
+                  </a>
+                </li>
+                <li>
+                  <a href='#'>
+                    <MdDashboard className="icon"/>
+                    <span className='as-span-text'>Audience</span>
+                    { toggle? (<IoIosArrowUp className='arrow' onClick={handleToggle} />):
+                        (<IoIosArrowDown className='arrow' onClick={handleToggle} />)}
+                  </a>
+                  <ul className={toggle ? 'assub-menu' : 'disabled'}>
+                    <li>
+                      <a href='#'>
+                        <MdDashboard className="icon"/>
+                        <span className='as-span-text'>Users</span>
+                      </a>
+                    </li>
+                    <li> 
+                      <a href='#'>
+                        <MdDashboard className="icon"/>
+                        <span className='as-span-text'>Subscribers</span>
+                      </a>
+                    </li>
+                  </ul>
                 </li>
               </ul>
-            </div>
-          ))}
+            </div> 
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    </>
+  )
+}
+
 
 export default AdminSidebar;
