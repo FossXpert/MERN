e3783036aa7c1919c700d1a7a7fe0fb0af35ea57
FossXpert <FossXpert@outlook.com>
2024-07-30 13:12:00 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Admin/Admin.tsx

--- 100x/udemy-pro/frontend-me/app/components/Admin/Admin.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Admin/Admin.tsx
@@ -3,13 +3,20 @@ import AdminSidebar from './Sidebar/AdminSidebar'
 
 import '../../css/css-admin/admin.css'
 import CreateCourse from './Courses/CreateCourse'
+import { useRouter } from 'next/router'
 type Props = {}
 
 const Admin = (props: Props) => {
+
+  const router = useRouter();
+
   return (
     <div className='admin-container'>
         <AdminSidebar/>
         <div className='admin-container-mini'>
+              {
+                
+              }
         </div>
     </div>
   )
