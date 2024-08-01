1963f202ac6f2d9d80ba6f5d97eb400ee0ee5bf5
rahulray8518 <rahulray8518@gmail.com>
2024-08-01 11:40:15 +0000

    kk

--- 100x/udemy-pro/frontend-me/app/components/Admin/Courses/CourseInformation.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Admin/Courses/CourseInformation.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../../css/css-admin/courseinformation.css'
 import { TextareaAutosize } from '@mui/material'
 
 type Props = {}
 
 const CourseInformation = (props: Props) => {
+
+  const [dragged,setDragged]  = useState(false);
+
   const handleFormSubmit = () => {
     console.log("Clicked form")
+  }
+  const handleDragOver = () => {
+    setDragged(true)
+  }
+
+  const handleDragLeave = () => {
+    setDragged(false)
+  }
+  const handleDrop = () => {
+    console.log("handle Drop")
   }
   return (
     <div className='course-information-container'>
@@ -64,6 +77,34 @@ const CourseInformation = (props: Props) => {
               cols={117} 
               className='input-textarea-2'
             />
+          </div>
+          <div className='group-3'>
+            <div className='group-3-1'>
+              <label htmlFor=''>Course Level</label>
+              <input
+              className='input-box-1'
+              type='text' 
+              name='course-price' 
+              placeholder='Please Enter Course Price' 
+              required
+            />
+            </div>
+            <div className='group-3-2'>
+              <label htmlFor=''>Demo Url</label>
+              <input
+              className='input-box-1'
+              type='text' 
+              name='course-estimated-price' 
+              placeholder='Please Enter Estimated Course Price' 
+              required
+            />
+            </div>
+          </div>
+          <div onDrop={handleDrop} onDragOver={handleDragOver} onDragLeave={handleDragLeave} className='group-5'>
+          <input className='hidden' name='file' type='file' accept='image/*' id=''/>
+          <label  htmlFor='file'>
+            <span>Drag and drop an image here</span>
+          </label>
           </div>
         </div>
     </div>
