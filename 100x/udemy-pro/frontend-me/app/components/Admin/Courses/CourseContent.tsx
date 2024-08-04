71ad0836e2d8d0c9ccab9f96563197e01504afdd
FossXpert <FossXpert@outlook.com>
2024-08-04 23:37:54 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Admin/Courses/CourseContent.tsx

--- 100x/udemy-pro/frontend-me/app/components/Admin/Courses/CourseContent.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Admin/Courses/CourseContent.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 type Props = {
-  
+  active : number;
+  setActive : (active:number) => void;
+  courseContentData : any;
+  setCourseContentData  : (courseContentData : any) => void; 
 }
 
 const CourseContent = (props: Props) => {
