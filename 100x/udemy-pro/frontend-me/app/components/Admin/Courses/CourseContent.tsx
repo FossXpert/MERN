0b7b410b63e9a060bd442f4d93841a89065a5966
FossXpert <FossXpert@outlook.com>
2024-08-05 12:19:46 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/Admin/Courses/CourseContent.tsx

--- 100x/udemy-pro/frontend-me/app/components/Admin/Courses/CourseContent.tsx
+++ 100x/udemy-pro/frontend-me/app/components/Admin/Courses/CourseContent.tsx
@@ -1,35 +1,31 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
 
 type Props = {
   active: number;
   setActive: (active: number) => void;
   courseContentData: {
-    videoUrl: "",
-    title: "",
-    description: "",
-    videoSection: "Untitled Section",
-    links: [
-      {
-        title: "",
-        url: ""
-      },
-    ],
-    suggestion: "",
+    videoUrl: string;
+    title: string;
+    description: string;
+    videoSection: string;
+    links: {
+      title: string;
+      url: string;
+    }[];
+    suggestion: string;
   }[];
-  setCourseContentData: (courseContentData:{
-    videoUrl: "",
-    title: "",
-    description: "",
-    videoSection: "Untitled Section",
-    links: [
-      {
-        title: "",
-        url: ""
-      },
-    ],
-    suggestion: "",
+  setCourseContentData: (courseContentData: {
+    videoUrl: string;
+    title: string;
+    description: string;
+    videoSection: string;
+    links: {
+      title: string;
+      url: string;
+    }[];
+    suggestion: string;
   }[]) => void;
-}
+};
 
 const CourseContent: FC<Props> = ({ active, setActive, courseContentData, setCourseContentData }) => {
 
@@ -38,18 +34,16 @@ const CourseContent: FC<Props> = ({ active, setActive, courseContentData, setCou
       <div className='flex w-full h-full
        items-center flex-col border border-solid border-blue-500'>
         <h1 className=''>Course Content</h1>
-        {/* Untitled Section k Main containera jiske end me buttonhoga */}
         <div className='flex w-3/4 flex-col h-auto border border-solid border-green-500'>
-          {
-          }
-          <div className='flex w-full h-full  border border-solid border-orange-500'>
-            
-          </div>
+          {/* Content rendering logic can be added here */}
+          hi
+          <div className='flex w-full h-full border border-solid border-orange-500'>
 
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default CourseContent
+export default CourseContent;
