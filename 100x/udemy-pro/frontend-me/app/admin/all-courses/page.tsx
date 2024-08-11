0b32bc81cf16f1dfa2f25958719dbb4823f6947b
FossXpert <FossXpert@outlook.com>
2024-08-11 23:59:34 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/admin/all-courses/page.tsx

--- 100x/udemy-pro/frontend-me/app/admin/all-courses/page.tsx
+++ 100x/udemy-pro/frontend-me/app/admin/all-courses/page.tsx
@@ -10,12 +10,14 @@ type Props = {
 
 const page:FC<Props> = ({active,setActive}) => {
   return (
-    <div className='flex'>
-        <div>
+    <div className='flex border border-solid border-black'>
+        <div className='flex w-[25%] border-solid border-black'>
             <AdminSidebar active={active} setActive={setActive}/>
         </div>
-        
+        <div className='flex w-[75%] border-solid border-black'>
         <AllCourses/>
+        </div>
+        
     </div>
   )
 }
