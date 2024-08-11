cb52dac41d1ee96c00c7da9055f137e098b29f6e
FossXpert <FossXpert@outlook.com>
2024-08-12 00:04:40 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/admin/all-courses/page.tsx

--- 100x/udemy-pro/frontend-me/app/admin/all-courses/page.tsx
+++ 100x/udemy-pro/frontend-me/app/admin/all-courses/page.tsx
@@ -3,6 +3,11 @@
 import AdminSidebar from '../../../app/components/Admin/Sidebar/AdminSidebar';
 import AllCourses from '../../components/Admin/Courses/AllCourses'
 import React, { FC } from 'react'
+import '../../css/css-admin/admin.css'
+import AdminHeader from '../../../app/components/Admin/AdminHeader';
+import { IoMdNotifications } from 'react-icons/io';
+import CreateCourse from '../../../app/components/Admin/Courses/CreateCourse';
+
 type Props = {
   active: number;
   setActive: (active: number) => void;
@@ -10,15 +15,22 @@ type Props = {
 
 const page:FC<Props> = ({active,setActive}) => {
   return (
-    <div className='flex border border-solid border-black'>
-        <div className='flex w-[25%] border-solid border-black'>
-            <AdminSidebar active={active} setActive={setActive}/>
+    <>
+    <div className="admin-container">
+      <AdminSidebar active={active} setActive={setActive} />
+      <div className='admin-container-mini'> 
+        <div className='admin-header'>
+          <AdminHeader/>
+          <div className="admin-header-notification">
+            <IoMdNotifications/>
+          </div>
         </div>
-        <div className='flex w-[75%] border-solid border-black'>
-        <AllCourses/>
+        <div className='admin-content'>
+            <AllCourses/>
         </div>
-        
+      </div>
     </div>
+    </>
   )
 }
 
