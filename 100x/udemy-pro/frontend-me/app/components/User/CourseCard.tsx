7868dd4ae9384973b0ad965070309d19d0563352
FossXpert <FossXpert@outlook.com>
2024-08-13 11:52:06 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/app/components/User/CourseCard.tsx

--- 100x/udemy-pro/frontend-me/app/components/User/CourseCard.tsx
+++ 100x/udemy-pro/frontend-me/app/components/User/CourseCard.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { FC } from 'react'
 
 type Props = {
-    
-
+    name : string;
+    price : string;
+    estimatedPrice : string;
+    tags : string;
+    thumbnail : string;
+    level : string;
+    demoUrl : string;
+    totalVideos : string;
 }
 
-const CourseCard = (props: Props) => {
+const CourseCard:FC<Props> = ({name,price,estimatedPrice,tags,thumbnail,level,demoUrl,totalVideos}) => {
   return (
-    <div>CourseCard</div>
+    <>
+        <div className='flex'></div>
+    </>
   )
 }
 
