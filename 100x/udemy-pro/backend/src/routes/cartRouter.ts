1751ce3c45d79112a3e7e87cc12f940b486d5327
FossXpert <FossXpert@outlook.com>
2024-09-11 11:57:50 +0530

    Auto commit: Updated files: 100x/udemy-pro/backend/src/routes/cartRouter.ts

--- 100x/udemy-pro/backend/src/routes/cartRouter.ts
+++ 100x/udemy-pro/backend/src/routes/cartRouter.ts
@@ -6,6 +6,6 @@ import { isAuthenticated } from '../middlewares/auth';
 const cartRouter = express.Router();
 
 cartRouter.post('/addtocart',isAuthenticated,addToCart);
-cartRouter.post('/removeFromCart',isAuthenticated,removeFromCart);
+cartRouter.post('/removefromcart',isAuthenticated,removeFromCart);
 
 export default cartRouter;
