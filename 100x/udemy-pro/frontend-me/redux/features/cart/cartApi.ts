edc1bae236e9433d2d90c908f579763618c00ad7
FossXpert <FossXpert@outlook.com>
2024-09-11 17:13:55 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/redux/features/cart/cartApi.ts

--- 100x/udemy-pro/frontend-me/redux/features/cart/cartApi.ts
+++ 100x/udemy-pro/frontend-me/redux/features/cart/cartApi.ts
@@ -2,8 +2,10 @@ import { apiSlice } from "../api/apiSlice";
 
 export const cartApi = apiSlice.injectEndpoints({
     endpoints : (builder) => ({
-        addToCart = builder.mutation({
-            
+        addToCart :  builder.mutation({
+            query : (data) => ({
+                url : 'cart/addtocart'
+            })
         })
     })
 })
