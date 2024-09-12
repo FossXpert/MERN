607408dd74ebfb385cbd599a26870665eb15be53
FossXpert <FossXpert@outlook.com>
2024-09-12 13:49:20 +0530

    Auto commit: Updated files: 100x/udemy-pro/frontend-me/redux/features/cart/cartApi.ts

--- 100x/udemy-pro/frontend-me/redux/features/cart/cartApi.ts
+++ 100x/udemy-pro/frontend-me/redux/features/cart/cartApi.ts
@@ -1,19 +1,35 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const cartApi = apiSlice.injectEndpoints({
-    endpoints : (builder) => ({
-        addToCart :  builder.mutation({
-            query : (id) => ({
-                url : 'cart/addtocart',
+    endpoints: (builder) => ({
+        addToCart: builder.mutation({
+            query: (id) => ({
+                url: 'cart/addtocart',
                 method: 'POST',
-                body : {
-                    _id:id
+                body: {
+                    _id: id
                 },
-                credentials : 'include' as const
+                credentials: 'include' as const
             })
         }),
-        remove
+        removeFromCart: builder.mutation({
+            query: (id) => ({
+                url: 'cart/removefromcart',
+                method: 'POST',
+                body: {
+                    _id: id
+                },
+                credentials: 'include' as const
+            })
+        }),
+        getCartStatus: builder.query({
+            query: () => ({
+                url: 'cart/getcartstatus',
+                method: 'GET',
+                credentials: 'include' as const
+            })
+        })
     })
 })
 
-export const {useAddToCartMutation} = cartApi;
+export const { useAddToCartMutation,useRemoveFromCartMutation,useGetCartStatusQuery } = cartApi;
